5caa24370ce68f672cd6302729ab4c68e5638a1b
Ritvick Lohani <50543918+ritlo@users.noreply.github.com>
2024-07-01 16:43:13 +0530

    cuda-inference test working

--- UniversalAdMute/models/mobileclipS2/mobileclip_s2.py
+++ UniversalAdMute/models/mobileclipS2/mobileclip_s2.py
@@ -19,9 +19,12 @@ class mobileclip_s2:
             'hf-hub:apple/MobileCLIP-S2-OpenCLIP', cache_dir=model_path)
         self.tokenizer = open_clip.get_tokenizer(
             'hf-hub:apple/MobileCLIP-S2-OpenCLIP')
-        self.device = torch.device(
-
-            "cpu")
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda")
+            print("Using CUDA")
+        else: 
+            self.device = torch.device("cpu")
+            print("Using CPU")
         self.model.to(self.device)
         self.model.eval()
 
@@ -80,39 +83,70 @@ class mobileclip_s2:
                     print(f"Text Prompt: {text_prompt}")
                     print(f"Similarity Score: {similarity[0][0]:.2f}%")
 
+    # def infer2(self):
+
+    #     image_folder = 'test_imgs/input'
+    #     image_files = [os.path.join(image_folder, f) for f in os.listdir(
+    #         image_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
+
+    #     text_prompts = ["advertisement", "football match tv broadcast"]
+
+    #     for image_path in image_files:
+
+    #         img = Image.open(image_path).convert('RGB')
+    #         img_tensor = self.preprocess_val(img).unsqueeze(0).to(self.device)
+
+    #         with torch.no_grad(), torch.amp.autocast('cuda'):
+
+    #             image_features = self.model.encode_image(img_tensor)
+    #             text_features = self.model.encode_text(
+    #                 self.tokenizer(text_prompts).to(self.device))
+
+    #             image_features = image_features / \
+    #                 image_features.norm(dim=-1, keepdim=True)
+    #             text_features = text_features / \
+    #                 text_features.norm(dim=-1, keepdim=True)
+
+    #             similarity_scores = image_features @ text_features.T
+    #             text_probs = (
+    #                 100.0 * similarity_scores.softmax(dim=-1)).squeeze(0)
+
+    #         print(f"Image Path: {image_path}")
+    #         for i, text_prompt in enumerate(text_prompts):
+    #             print(
+    #                 f"Similarity to '{text_prompt}': {text_probs[i].item():.2f}%")
+    #         print("\n")
+
     def infer2(self):
 
-        uris = [
-            "https://i0.wp.com/becleverwithyourcash.com/wp-content/uploads/2021/08/watch_football_on_TV.jpeg",
-            "http://images.cocodataset.org/test-stuff2017/000000024309.jpg",
-            "https://ultralytics.com/images/zidane.jpg"
-        ]
+        image_folder = 'test_imgs/input'
+        image_files = [os.path.join(image_folder, f) for f in os.listdir(
+            image_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
 
-        text_prompts = ["cat", "football match tv broadcast"]
+        text_prompts = ["advertisement", "football match tv broadcast"]
+        while True:
+            for image_path in image_files:
 
-        for uri in uris:
+                img = Image.open(image_path).convert('RGB')
+                img_tensor = self.preprocess_val(img).unsqueeze(0).to(self.device)
 
-            response = requests.get(uri, stream=True)
-            image = self.preprocess_val(Image.open(response.raw).convert(
-                'RGB')).unsqueeze(0).to(self.device)
+                with torch.no_grad(), torch.amp.autocast('cuda'):
 
-            with torch.no_grad(), torch.cuda.amp.autocast():
+                    image_features = self.model.encode_image(img_tensor)
+                    text_features = self.model.encode_text(
+                        self.tokenizer(text_prompts).to(self.device))
 
-                image_features = self.model.encode_image(image)
-                text_features = self.model.encode_text(
-                    self.tokenizer(text_prompts).to(self.device))
+                    image_features = image_features / \
+                        image_features.norm(dim=-1, keepdim=True)
+                    text_features = text_features / \
+                        text_features.norm(dim=-1, keepdim=True)
 
-                image_features = image_features / \
-                    image_features.norm(dim=-1, keepdim=True)
-                text_features = text_features / \
-                    text_features.norm(dim=-1, keepdim=True)
+                    similarity_scores = image_features @ text_features.T
+                    text_probs = (
+                        100.0 * similarity_scores.softmax(dim=-1)).squeeze(0)
 
-                similarity_scores = image_features @ text_features.T
-                text_probs = (
-                    100.0 * similarity_scores.softmax(dim=-1)).squeeze(0)
-
-            print(f"Image URL: {uri}")
-            for i, text_prompt in enumerate(text_prompts):
-                print(
-                    f"Similarity to '{text_prompt}': {text_probs[i].item():.2f}%")
-            print("\n")
+                print(f"Image Path: {image_path}")
+                for i, text_prompt in enumerate(text_prompts):
+                    print(
+                        f"Similarity to '{text_prompt}': {text_probs[i].item():.2f}%")
+                print("\n")
---
+++ open_cliptest.py
@@ -0,0 +1,20 @@
+import torch
+from PIL import Image
+import open_clip
+
+model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k',cache_dir=r'test-cache')
+model.eval()  # model in train mode by default, impacts some models with BatchNorm or stochastic depth active
+tokenizer = open_clip.get_tokenizer('ViT-B-32')
+
+image = preprocess(Image.open("test_imgs/input/soccer-worldcup-qat-ecu-report-sixteen_nine.jpg")).unsqueeze(0)
+text = tokenizer(["cat","advertisement"])
+
+with torch.no_grad(), torch.amp.autocast("cuda"):
+    image_features = model.encode_image(image)
+    text_features = model.encode_text(text)
+    image_features /= image_features.norm(dim=-1, keepdim=True)
+    text_features /= text_features.norm(dim=-1, keepdim=True)
+
+    text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+
+print("Label probs:", text_probs)  # prints: [[1., 0., 0.]]
