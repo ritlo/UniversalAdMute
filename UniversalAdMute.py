1275edac76ffff5f3b47e312829d848c9ab0841a
Ritvick Lohani <50543918+ritlo@users.noreply.github.com>
2024-07-03 23:46:17 +0530

    Add text prompts to UniversalAdMute.py

--- UniversalAdMute.py
+++ UniversalAdMute.py
@@ -6,12 +6,14 @@ from time import sleep
 
 audioCont = AudioController()
 model = mobileclip_s2()
+text_prompts = ["Television tv advertisement break",
+                "football soccer fifa uefa match tv sports broadcast"] # Set second description to the content you intend to watch.
 
 while True:
-    probs = model.infer(simpleScreenshot())
+    probs = model.infer(simpleScreenshot(), text_prompts)
     if probs[1] > 51 and audioCont.isUnmuted == False:
         audioCont.unmute()
-        
+
     elif probs[0] > 50:
         audioCont.mute()
 
